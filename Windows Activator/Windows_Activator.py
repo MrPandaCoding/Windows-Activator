921c3a1cf2e170a95f11d61e8a5606a6807a635b
noah weeks <noahweeks1111@gmail.com>
2022-01-04 17:29:48 -0800

    Main code

--- Windows Activator/Windows_Activator.py
+++ Windows Activator/Windows_Activator.py
@@ -1,1 +1,19 @@
+from os import system
+import time
+
+x = "Windows Activator By: Noah W."
+
+system("title " + x)
+
+print("=====================================================""\n#Project: Activation of Windows Software products.\n""=====================================================\n")
+print("#Supported products:\n""- Windows 10 Home\n- Windows 10 Professional\n")
+print("=====================================================")
+time.sleep(1.5)
+print("Activating Windows...")
+time.sleep(1.5)
+print("please wait...")
+time.sleep(0.5)
+print("=====================================================")
+
+
 
