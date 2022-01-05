c57e8952defea397435b6738b6e7a48045b430cc
noah weeks <noahweeks1111@gmail.com>
2022-01-04 18:08:05 -0800

    Finished Product

--- Windows Activator/Windows_Activator.py
+++ Windows Activator/Windows_Activator.py
@@ -1,5 +1,7 @@
+import os
 from os import system
 import time
+import ctypes, sys
 
 x = "Windows Activator By: Noah W."
 
@@ -15,5 +17,17 @@ print("please wait...")
 time.sleep(0.5)
 print("=====================================================")
 
-
-
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+if is_admin():
+    os.system('cmd /c "slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99"')
+    os.system('cmd /c "slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX"')
+    os.system('cmd /c "slmgr /skms s7.uk.to"')
+    os.system('cmd /c "slmgr /skms s8.uk.to"')
+    os.system('cmd /c "slmgr /skms s9.uk.to"')
+    os.system('cmd /c "slmgr /ato"')
+else:
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
